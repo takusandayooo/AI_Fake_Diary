04c28da2b8e1c7b315996459de2e6116b433d3e7
たくや <takuyadayo014@gmail.com>
2025-02-15 20:31:21 +0900

    feat: Appleシリコンでのパソコンで

--- module/create_image.py
+++ module/create_image.py
@@ -2,7 +2,7 @@ from diffusers import StableDiffusionImg2ImgPipeline, DPMSolverMultistepSchedule
 import torch
 from PIL import Image
 
-def create_image(img_path):
+def create_image(img_path,pc):
     # 利用したいAIモデル
     model_id = "CompVis/stable-diffusion-v1-4"
 
@@ -12,7 +12,7 @@ def create_image(img_path):
 
     # GPUを使うように変更
     try:
-        pipeline = pipeline.to("cuda")
+        pipeline = pipeline.to(pc)
     except:
         print("No GPU found, using CPU")
     init_image = Image.open(img_path)
